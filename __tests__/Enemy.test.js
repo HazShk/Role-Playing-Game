5b9f9f50b4640f7b28bafe4165588854caa6ebb6
Hassan Shaikh <shaikh.hassan01@outlook.com>
2022-04-25 13:10:00 -0400

    Converted ES5 Object create to ES6 Classes, introduced super method. Code Completed

--- __tests__/Enemy.test.js
+++ __tests__/Enemy.test.js
@@ -13,6 +13,7 @@ test("creates an enemy object", () => {
   expect(enemy.agility).toEqual(expect.any(Number));
   expect(enemy.potion).toEqual(expect.any(Object));
 });
+
 test("gets enemy's health value", () => {
   const enemy = new Enemy("goblin", "sword");
 
@@ -21,16 +22,27 @@ test("gets enemy's health value", () => {
   );
 });
 
+test("gets a description of the enemy", () => {
+  const enemy = new Enemy("goblin", "sword");
+
+  expect(enemy.getDescription()).toEqual(expect.stringContaining("goblin"));
+  expect(enemy.getDescription()).toEqual(expect.stringContaining("sword"));
+});
+
 test("checks if enemy is alive or not", () => {
   const enemy = new Enemy("goblin", "sword");
+
   expect(enemy.isAlive()).toBeTruthy();
+
   enemy.health = 0;
+
   expect(enemy.isAlive()).toBeFalsy();
 });
 
 test("gets enemy's attack value", () => {
   const enemy = new Enemy("goblin", "sword");
   enemy.strength = 10;
+
   expect(enemy.getAttackValue()).toBeGreaterThanOrEqual(5);
   expect(enemy.getAttackValue()).toBeLessThanOrEqual(15);
 });
@@ -38,8 +50,12 @@ test("gets enemy's attack value", () => {
 test("subtracts from enemy's health", () => {
   const enemy = new Enemy("goblin", "sword");
   const oldHealth = enemy.health;
+
   enemy.reduceHealth(5);
+
   expect(enemy.health).toBe(oldHealth - 5);
+
   enemy.reduceHealth(99999);
+
   expect(enemy.health).toBe(0);
 });
